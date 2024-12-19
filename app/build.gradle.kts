53de433f9d027059fd386fb3f58f771703eeae43
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2024-12-19 09:14:25 +0300

    commit before add new feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs["release"]
-            isMinifyEnabled = true
+            isMinifyEnabled = false                     // TODO for develop branch
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
