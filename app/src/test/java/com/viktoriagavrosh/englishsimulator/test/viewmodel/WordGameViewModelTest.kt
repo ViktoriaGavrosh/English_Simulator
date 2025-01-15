387d3d068d6916f067130ad12c97cbead1066f6d
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2025-01-15 15:41:56 +0300

    WordGameViewModelTest.kt added

---
+++ app/src/test/java/com/viktoriagavrosh/englishsimulator/test/viewmodel/WordGameViewModelTest.kt
@@ -0,0 +1,94 @@
+package com.viktoriagavrosh.englishsimulator.test.viewmodel
+
+import com.viktoriagavrosh.englishsimulator.fake.FakeSource
+import com.viktoriagavrosh.englishsimulator.fake.repositories.FakeWordRepository
+import com.viktoriagavrosh.englishsimulator.model.Word
+import com.viktoriagavrosh.englishsimulator.ui.features.screens.game.model.toWord
+import com.viktoriagavrosh.englishsimulator.ui.features.word.WordGameViewModel
+import com.viktoriagavrosh.englishsimulator.utils.RequestResult
+import com.viktoriagavrosh.englishsimulator.utils.TestDispatcherRule
+import com.viktoriagavrosh.englishsimulator.utils.toWord
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertNotEquals
+import org.junit.Rule
+import org.junit.Test
+
+class WordGameViewModelTest {
+
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    private val fakeWords = FakeSource.fakeWordsDb.map { it.toWord() }
+
+    @Test
+    fun wordGameViewModel_initUiState_initGameQuestion() {
+        runTest {
+            val theme = FakeSource.fakeWordsDb[0].theme
+            val isToEnglish = true
+            val viewModel = initViewModel(
+                requestResult = RequestResult.Success(fakeWords),
+                theme = theme,
+                isToEnglish = isToEnglish
+            )
+            val actualGameQuestion = viewModel.uiState.first().gameQuestion
+            assert(actualGameQuestion.toWord(theme, isToEnglish) in fakeWords)
+        }
+    }
+
+    @Test
+    fun wordGameViewModel_initUiState_initFieldIsError() {
+        runTest {
+            val viewModel = initViewModel(
+                requestResult = RequestResult.Success(fakeWords),
+            )
+            val actualValueIsErrorField = viewModel.uiState.first().isError
+            assert(!actualValueIsErrorField)
+        }
+    }
+
+    @Test
+    fun wordGameViewModel_initUiStateWithError_initFieldIsError() {
+        runTest {
+            val viewModel = initViewModel(RequestResult.Error())
+            val actualValueIsErrorField = viewModel.uiState.first().isError
+            assert(actualValueIsErrorField)
+        }
+    }
+
+    @Test
+    fun wordGameViewModel_updateUiState_gameQuestionUpdated() { // sometimes failed because updateUiState() contains random()
+        runTest {
+            val viewModel = initViewModel(
+                requestResult = RequestResult.Success(fakeWords)
+            )
+            val oldGameQuestion = viewModel.uiState.first().gameQuestion
+            viewModel.updateUiState()
+            val actualGameQuestion = viewModel.uiState.first().gameQuestion
+            assertNotEquals(oldGameQuestion, actualGameQuestion)
+        }
+    }
+
+    @Test
+    fun wordGameViewModel_updateUiState_countUpdated() {
+        runTest {
+            val viewModel = initViewModel(RequestResult.Success(fakeWords))
+            val oldCount = viewModel.uiState.first().score
+            viewModel.updateUiState()
+            val actualCount = viewModel.uiState.first().score
+            assertNotEquals(oldCount, actualCount)
+        }
+    }
+
+    private fun initViewModel(
+        requestResult: RequestResult<List<Word>>,
+        theme: String = FakeSource.fakeWordsDb[0].theme,
+        isToEnglish: Boolean = true,
+    ): WordGameViewModel {
+        return WordGameViewModel(
+            repository = FakeWordRepository(requestResult),
+            theme = theme,
+            isToEnglish = isToEnglish,
+        )
+    }
+}
