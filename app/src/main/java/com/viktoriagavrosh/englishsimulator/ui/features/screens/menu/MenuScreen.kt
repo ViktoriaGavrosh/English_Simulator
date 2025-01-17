aab671a12c0f76e163311a41563b427fde545a15
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2025-01-17 19:06:06 +0300

    NavigationTest.kt fixed

--- app/src/main/java/com/viktoriagavrosh/englishsimulator/ui/features/screens/menu/MenuScreen.kt
+++ app/src/main/java/com/viktoriagavrosh/englishsimulator/ui/features/screens/menu/MenuScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.viktoriagavrosh.englishsimulator.R
