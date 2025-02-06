27092e446db43e22b237d4361936836ac69e1371
ViktoriaGavrosh <viktoria.gavrosh@gmail.com>
2025-02-06 14:12:14 +0300

    Dependency for Datastore added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.material3.window.size)
 
+    implementation(libs.datastore.preferences)
+
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
 
